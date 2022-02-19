10bcf372042a3e0af57d9a5cdd6d89979bdfb3e5
Yihao Mai <ma1h01@s172-20-137-h129.paws.uga.edu>
2022-02-19 14:02:15 -0500

    Initial Login Interface

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,24 +6,63 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import java.util.ArrayList;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton EnterCode;
+    private ArrayList<String> loginCodes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EnterCode=(ImageButton) findViewById(R.id.imageButton2);
+        EnterCode= findViewById(R.id.imageButton2);
+        loginCodes = new ArrayList<>();
+        loginCodes.add("123456");
         EnterCode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity2();
+                authentication();
             }
         });
+    }
+
+    private void authentication () {
+        EditText code =  findViewById(R.id.editTextTextPassword);
+        String temp = code.getText().toString();
+        if (loginCodes.contains(temp)) {
+            openActivity2();
+        } else {
+            promptUser();
+        }
+
     }
     public void openActivity2(){
         Intent intent=new Intent(this, MainActivity2.class);
         startActivity(intent);
+    }
+
+    private void promptUser() {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("You have entered the wrong code.");
+        alertDialogBuilder.setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                System.exit(0);
+            }
+        });
+
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
     }
 }
